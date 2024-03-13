c82cd522bce294579a33af6663bddbedec2247ee
yvromero <yvonne.romero@uds.edu.py>
2024-03-13 00:08:25 -0300

    Add CRUD API

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,9 +1,10 @@
-import express from 'express';
+import express, { Router } from 'express';
 import path from 'path';
 
 
 interface Options {
     port: number;
+    routes: Router;
     public_path?: string;
 }
 
@@ -12,22 +13,29 @@ export class Server {
     private app = express();
     private readonly port: number;
     private readonly publicPath: string;
+    private readonly routes: Router;
 
 
     constructor( options: Options ) {
-        const { port, public_path = 'public' } = options;
+        const { port, routes, public_path = 'public' } = options;
         this.port = port;
         this.publicPath = public_path;
+        this.routes = routes;
     }
 
     async start() {
 
+        //* Middlewares
+        this.app.use( express.json() ); // raw
+        this.app.use( express.urlencoded({ extended: true }) ); // x-www-form-urlencoded
 
-        // * Middlewares
-
-        // * Public Folder
+        //* Public Folder
         this.app.use( express.static( this.publicPath ) );
 
+        //* Routes
+        this.app.use( this.routes )
+
+        //* SPA
         this.app.get('*', (req, res) => {
             const indexPath = path.join( __dirname + `../../../${ this.publicPath }/index.html`);
             res.sendFile(indexPath);
